64e4f6a8d146b85bf7c1dbe0a0e579589ba39199
GSystems <iulian.glk@gmail.com>
2017-12-18 01:05:29 +0200

    [IG] Small fix for the managed bean

--- src/main/java/pf/MapBean.java
+++ src/main/java/pf/MapBean.java
@@ -59,4 +59,12 @@ public class MapBean implements Serializable {
 		this.tweetFacade = tweetFacade;
 	}
 
+	public MapModel getMapModel() {
+		return mapModel;
+	}
+
+	public void setMapModel(MapModel mapModel) {
+		this.mapModel = mapModel;
+	}
+
 }
