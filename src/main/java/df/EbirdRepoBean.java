41eae5653ad59dbcba5a8a545f758393f95d7715
GSystems <iulian.glk@gmail.com>
2018-01-09 09:37:15 +0200

    [IG] merge problem

--- src/main/java/df/EbirdRepoBean.java
+++ src/main/java/df/EbirdRepoBean.java
@@ -1,11 +1,13 @@
 package main.java.df;
 
+import java.util.List;
 import java.util.concurrent.Future;
 
 import javax.ejb.AsyncResult;
 import javax.inject.Inject;
 
 import main.java.df.mapper.EbirdMapper;
+import main.java.df.model.EBirdData;
 import main.java.df.model.EBirdRequest;
 import main.java.df.model.EBirdResponse;
 import main.java.rf.ebird.EbirdsServiceClient;
@@ -21,6 +23,17 @@ public class EbirdRepoBean implements EbirdRepo {
 		ebirdsService = new EbirdsServiceClientBean();
 		return new AsyncResult<>(EbirdMapper.toEbirdsResponseFromWrapper(
 				ebirdsService.retrieveEBirdData(EbirdMapper.fromEBirdRequestToWrapper(request))));
+	}
+
+	@Override
+	public void insertEbirdData(List<EBirdData> ebirds) {
+		
+	}
+
+	@Override
+	public Future<List<EBirdData>> retrieveEbirdDataFromDb() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
